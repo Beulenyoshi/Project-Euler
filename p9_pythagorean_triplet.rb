88ee84431b0f702f02d936543f0e007d0fa7009b
Thomas Johannesmeyer <thomas@geeky.gent>
2017-09-03 03:38:33 +0200

    Problem 9 solved

--- p9_pythagorean_triplet.rb
+++ p9_pythagorean_triplet.rb
@@ -15,16 +15,54 @@ this stuff is worth it, you can buy me a beer or coffee in return
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
 a2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 
 =end
 
+require 'Progress_Bar'
+
+
 def isTriplet?(tuple)
   if tuple.count < 3 then return false end
   (tuple[0]**2 + tuple[1]**2) == tuple[2]**2
 end
 
-puts isTriplet?([3,4,5])
+def generate_triplets()
+  puts "Progress bar represents worst case time:"
+  bar = ProgressBar.new()
+
+  # Since a < b < c the three loops start above the index of the outer loop
+  (1..998).each do |a|
+    if (a % 10 == 0) then
+      bar.increment!
+    end
+    ((a + 1)..999).each do |b|
+
+      # The following logic helps us sorting out every second element.
+      # Goal is to find a triplet which sums up to 1000.
+      # If the sum of the first two numbers is odd no even number will be valid and vice versa
+
+      start_index = b + 1
+      if (a + b).even? then
+        start_index = start_index.even? ? start_index : start_index + 1
+      elsif
+        start_index = start_index.even? ? start_index + 1 : start_index
+      end
+
+      (start_index..1000).step(2) do |c|
+        if (isTriplet?([a,b,c])) then
+          if (a+b+c == 1000) then
+            return [a,b,c]
+          end
+        end
+      end
+    end
+  end
+end
+
+
+# puts isTriplet?([3,4,5])
+puts generate_triplets()
